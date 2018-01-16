290013ce6833da4f63f0d0fcf871fc27049ae941
ruben muñoz <ruben7461@msn.com>
2018-01-16 22:53:38 +0100

    he creado las consultas necesarias para la salida de los datos de los pokemon

--- Pokedex/Form1.Designer.cs
+++ Pokedex/Form1.Designer.cs
@@ -29,22 +29,227 @@
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.idPoke = new System.Windows.Forms.Label();
+            this.lblTipo = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.lblFelicidad = new System.Windows.Forms.Label();
+            this.lblAltura = new System.Windows.Forms.Label();
+            this.lblPeso = new System.Windows.Forms.Label();
+            this.lblColour = new System.Windows.Forms.Label();
+            this.lblGeneracion = new System.Windows.Forms.Label();
+            this.lblHabitat = new System.Windows.Forms.Label();
+            this.lblColor = new System.Windows.Forms.Label();
+            this.lblFoto = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersVisible = false;
             this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.MultiSelect = false;
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(130, 375);
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dataGridView1.Size = new System.Drawing.Size(122, 371);
             this.dataGridView1.TabIndex = 0;
+            // 
+            // idPoke
+            // 
+            this.idPoke.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.idPoke.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.idPoke.Location = new System.Drawing.Point(308, 12);
+            this.idPoke.Name = "idPoke";
+            this.idPoke.Size = new System.Drawing.Size(72, 57);
+            this.idPoke.TabIndex = 1;
+            this.idPoke.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblTipo
+            // 
+            this.lblTipo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblTipo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTipo.Location = new System.Drawing.Point(587, 12);
+            this.lblTipo.Name = "lblTipo";
+            this.lblTipo.Size = new System.Drawing.Size(107, 34);
+            this.lblTipo.TabIndex = 3;
+            this.lblTipo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(369, 113);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(170, 34);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "habbitat";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(369, 164);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(170, 34);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "felicidad";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(369, 215);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(170, 34);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "altura";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            this.label7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(369, 264);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(170, 34);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "peso";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label8
+            // 
+            this.label8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(369, 360);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(170, 34);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "generación";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label9
+            // 
+            this.label9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(369, 313);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(170, 34);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "color";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblFelicidad
+            // 
+            this.lblFelicidad.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblFelicidad.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFelicidad.Location = new System.Drawing.Point(558, 164);
+            this.lblFelicidad.Name = "lblFelicidad";
+            this.lblFelicidad.Size = new System.Drawing.Size(136, 34);
+            this.lblFelicidad.TabIndex = 11;
+            this.lblFelicidad.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblAltura
+            // 
+            this.lblAltura.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblAltura.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAltura.Location = new System.Drawing.Point(558, 215);
+            this.lblAltura.Name = "lblAltura";
+            this.lblAltura.Size = new System.Drawing.Size(136, 34);
+            this.lblAltura.TabIndex = 13;
+            this.lblAltura.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblPeso
+            // 
+            this.lblPeso.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblPeso.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPeso.Location = new System.Drawing.Point(558, 263);
+            this.lblPeso.Name = "lblPeso";
+            this.lblPeso.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lblPeso.Size = new System.Drawing.Size(136, 34);
+            this.lblPeso.TabIndex = 14;
+            this.lblPeso.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblColour
+            // 
+            this.lblColour.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblColour.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblColour.Location = new System.Drawing.Point(558, 313);
+            this.lblColour.Name = "lblColour";
+            this.lblColour.Size = new System.Drawing.Size(136, 34);
+            this.lblColour.TabIndex = 15;
+            this.lblColour.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblGeneracion
+            // 
+            this.lblGeneracion.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblGeneracion.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGeneracion.Location = new System.Drawing.Point(558, 360);
+            this.lblGeneracion.Name = "lblGeneracion";
+            this.lblGeneracion.Size = new System.Drawing.Size(136, 34);
+            this.lblGeneracion.TabIndex = 16;
+            this.lblGeneracion.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblHabitat
+            // 
+            this.lblHabitat.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblHabitat.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHabitat.Location = new System.Drawing.Point(558, 113);
+            this.lblHabitat.Name = "lblHabitat";
+            this.lblHabitat.Size = new System.Drawing.Size(136, 34);
+            this.lblHabitat.TabIndex = 10;
+            this.lblHabitat.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblColor
+            // 
+            this.lblColor.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblColor.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblColor.Location = new System.Drawing.Point(432, 12);
+            this.lblColor.Name = "lblColor";
+            this.lblColor.Size = new System.Drawing.Size(107, 34);
+            this.lblColor.TabIndex = 2;
+            this.lblColor.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblFoto
+            // 
+            this.lblFoto.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblFoto.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFoto.Location = new System.Drawing.Point(153, 12);
+            this.lblFoto.Name = "lblFoto";
+            this.lblFoto.Size = new System.Drawing.Size(128, 121);
+            this.lblFoto.TabIndex = 17;
+            this.lblFoto.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(568, 561);
+            this.ClientSize = new System.Drawing.Size(797, 561);
+            this.Controls.Add(this.lblFoto);
+            this.Controls.Add(this.lblGeneracion);
+            this.Controls.Add(this.lblColour);
+            this.Controls.Add(this.lblPeso);
+            this.Controls.Add(this.lblAltura);
+            this.Controls.Add(this.lblFelicidad);
+            this.Controls.Add(this.lblHabitat);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblTipo);
+            this.Controls.Add(this.lblColor);
+            this.Controls.Add(this.idPoke);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -56,6 +261,22 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label idPoke;
+        private System.Windows.Forms.Label lblTipo;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label lblFelicidad;
+        private System.Windows.Forms.Label lblAltura;
+        private System.Windows.Forms.Label lblPeso;
+        private System.Windows.Forms.Label lblColour;
+        private System.Windows.Forms.Label lblGeneracion;
+        private System.Windows.Forms.Label lblHabitat;
+        private System.Windows.Forms.Label lblColor;
+        private System.Windows.Forms.Label lblFoto;
     }
 }
 
--- Pokedex/Form1.cs
+++ Pokedex/Form1.cs
@@ -20,7 +20,28 @@ namespace Pokedex
         private static MySqlCommand comando;
         private string consulta;
         private MySqlDataReader resultado;
+        private MySqlDataReader resultado1;
+        private MySqlDataReader resultado2;
+        private MySqlDataReader resultado3;
+        private MySqlDataReader resultado4;
+        private MySqlDataReader resultado5;
+        private MySqlDataReader resultado6;
+        private MySqlDataReader resultado7;
+        private MySqlDataReader resultado8;
+        private MySqlDataReader resultado9;
+      
         private DataTable datos = new DataTable();
+      
+        private static MySqlCommand consultaNivel;
+        private static MySqlCommand consultaTipo;
+        private static MySqlCommand altura;
+        private static MySqlCommand peso;
+        private static MySqlCommand color;
+        private static MySqlCommand generacion;
+        private static MySqlCommand felicidad;
+        private static MySqlCommand idPokemon;
+        private static MySqlCommand habbitat;
+
 
         public Form1()
         {
@@ -32,9 +53,49 @@ namespace Pokedex
             comando = new MySqlCommand("Select name from pokemon", conexion);
             resultado = comando.ExecuteReader();
             datos.Load(resultado);
-            conexion.Close();
             dataGridView1.DataSource = datos;
-            
+            foreach (DataGridViewCell cell in dataGridView1.SelectedCells)
+            {
+                String prueba1 = cell.Value.ToString();
+                Console.WriteLine(prueba1);
+                idPokemon = new MySqlCommand("Select id from pokemon where name =" +prueba1, conexion);
+                consultaNivel = new MySqlCommand("Select base_experience from pokemon where name =" +prueba1, conexion);
+                consultaTipo = new MySqlCommand("Select species from pokemon where name =" +prueba1, conexion);
+                altura = new MySqlCommand("Select height from pokemon where name =" +prueba1, conexion);
+                peso = new MySqlCommand("Select weight from pokemon where name =" +prueba1, conexion);
+                color = new MySqlCommand("Select color from pokemon where name =" +prueba1, conexion);
+                generacion = new MySqlCommand("Select generation_id from pokemon where name =" +prueba1, conexion);
+                felicidad = new MySqlCommand("Select base_happiness from pokemon where name =" +prueba1, conexion);
+                habbitat = new MySqlCommand("Select habitat  from pokemon where name =" +prueba1, conexion);
+                resultado1 = idPokemon.ExecuteReader();
+                resultado2 = consultaNivel.ExecuteReader();
+                resultado3 = consultaTipo.ExecuteReader();
+                resultado4 = altura.ExecuteReader();
+                resultado5 = peso.ExecuteReader();
+                resultado6 = color.ExecuteReader();
+                resultado7 = generacion.ExecuteReader();
+                resultado8 = felicidad.ExecuteReader();
+                resultado9 = habbitat.ExecuteReader();
+                 idPoke.Text = resultado1["id"] as String;
+                lblColor.Text = resultado6["color"] as String;
+                lblTipo.Text = resultado6["species"] as String;
+                lblAltura.Text = resultado6["height"] as String;
+                lblPeso.Text = resultado6["weight"] as String;
+                lblGeneracion.Text = resultado6["generation_id"] as String;
+                lblFelicidad.Text = resultado6["base_happiness"] as String;
+                lblHabitat.Text = resultado6["habitat"] as String;
+              
+               
+
+
+            }
+
+
+            conexion.Close();
+
+
+
+
         }
     }
 }
