a0f9b0649654d4bb08e5f9c3780c4163a7f84750
xp <xp@CETYSDAM034>
2018-01-18 11:58:01 +0100

    corregido correctamente,funcionando

--- Pokedex/Form1.Designer.cs
+++ Pokedex/Form1.Designer.cs
@@ -52,12 +52,13 @@
             // 
             this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
             this.dataGridView1.ColumnHeadersVisible = false;
             this.dataGridView1.Location = new System.Drawing.Point(12, 12);
             this.dataGridView1.MultiSelect = false;
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.RowHeadersVisible = false;
             this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
             this.dataGridView1.Size = new System.Drawing.Size(122, 371);
             this.dataGridView1.TabIndex = 0;
--- Pokedex/Form1.cs
+++ Pokedex/Form1.cs
@@ -18,31 +18,17 @@ namespace Pokedex
 
         private MySqlConnection conexion;
         private MySqlConnection conexion2;
+
         private static MySqlCommand comando;
+        private static MySqlCommand comando2;
         private string consulta;
         private MySqlDataReader resultado;
-        private MySqlDataReader resultado1;
         private MySqlDataReader resultado2;
-        private MySqlDataReader resultado3;
-        private MySqlDataReader resultado4;
-        private MySqlDataReader resultado5;
-        private MySqlDataReader resultado6;
-        private MySqlDataReader resultado7;
-        private MySqlDataReader resultado8;
-        private MySqlDataReader resultado9;
-      
+
+
         private DataTable datos = new DataTable();
       
-        private static MySqlCommand consultaNivel;
-        private static MySqlCommand consultaTipo;
-        private static MySqlCommand altura;
-        private static MySqlCommand peso;
-        private static MySqlCommand color;
-        private static MySqlCommand generacion;
-        private static MySqlCommand felicidad;
-        private static MySqlCommand idPokemon;
-        private static MySqlCommand habbitat;
-
+       
 
         public Form1()
         {
@@ -55,9 +41,9 @@ namespace Pokedex
             comando = new MySqlCommand("Select name from pokemon", conexion);
             resultado = comando.ExecuteReader();
             datos.Load(resultado);
-            conexion.Close();
+            
             dataGridView1.DataSource = datos;
-
+            conexion.Close();
            
 
         }
@@ -68,56 +54,31 @@ namespace Pokedex
         {
             foreach (DataGridViewCell cell in dataGridView1.SelectedCells)
             {
+
                 conexion2.Open();
+              
                 String prueba1 = cell.Value.ToString();
-                Console.WriteLine(prueba1);
-                idPokemon = new MySqlCommand("Select id from pokemon where name=  '+prueba1'", conexion2);
-                consultaNivel = new MySqlCommand("Select base_experience from pokemon where name = '+prueba1'", conexion2);
-                consultaTipo = new MySqlCommand("Select species from pokemon where name = '+prueba1'", conexion2);
-                altura = new MySqlCommand("Select height from pokemon where name = '+prueba1'", conexion2);
-                peso = new MySqlCommand("Select weight from pokemon where name = '+prueba1'", conexion2);
-                color = new MySqlCommand("Select color from pokemon where name = '+prueba1'", conexion2);
-                generacion = new MySqlCommand("Select generation_id from pokemon where name = '+prueba1'", conexion2);
-                felicidad = new MySqlCommand("Select base_happiness from pokemon where name = '+prueba1'", conexion2);
-                habbitat = new MySqlCommand("Select habitat  from pokemon where name = '+prueba1'", conexion2);
-                resultado1 = idPokemon.ExecuteReader();
-                conexion2.Close();
-                conexion2.Open();
-                resultado2 = consultaNivel.ExecuteReader();
-                conexion2.Close();
-                conexion2.Open();
-                resultado3 = consultaTipo.ExecuteReader();
-                conexion2.Close();
-                conexion2.Open();
-                resultado4 = altura.ExecuteReader();
-                conexion2.Close();
-                conexion2.Open();
-                resultado5 = peso.ExecuteReader();
-                conexion2.Close();
-                conexion2.Open();
-                resultado6 = color.ExecuteReader();
-                conexion2.Close();
-                conexion2.Open();
-                resultado7 = generacion.ExecuteReader();
-                conexion2.Close();
-                conexion2.Open();
-                resultado8 = felicidad.ExecuteReader();
-                conexion2.Close();
-                conexion2.Open();
-                resultado9 = habbitat.ExecuteReader();
-                conexion2.Close();
-                conexion2.Open();
-                idPoke.Text = resultado1["id"] as String;
-                lblColor.Text = resultado6["base_experience"] as String;
-                lblTipo.Text = resultado6["species"] as String;
-                lblAltura.Text = resultado6["height"] as String;
-                lblPeso.Text = resultado6["weight"] as String;
-                lblGeneracion.Text = resultado6["generation_id"] as String;
-                lblFelicidad.Text = resultado6["base_happiness"] as String;
-                lblHabitat.Text = resultado6["habitat"] as String;
-                conexion2.Close();
-                Console.WriteLine(resultado6[""] as String);
+                
+                comando2  = new MySqlCommand("Select * from pokemon where name ='"+ prueba1 +"'", conexion2);
+                resultado2 = comando2.ExecuteReader();
+
+
+                if (resultado2.Read())
+                {
+                    idPoke.Text = resultado2["id"].ToString();
+                    lblColor.Text = resultado2["color"].ToString();
+                    lblTipo.Text = resultado2["species"].ToString();
+                    lblAltura.Text = resultado2["height"].ToString();
+                    lblPeso.Text = resultado2["weight"].ToString();
+                    lblGeneracion.Text = resultado2["generation_id"].ToString();
+                    lblFelicidad.Text = resultado2["base_happiness"].ToString();
+                    lblHabitat.Text = resultado2["habitat"].ToString();
+                }
+                
+                //Console.WriteLine(resultado6[""] as String);
             }
+            conexion2.Close();
         }
+
     }
 }
